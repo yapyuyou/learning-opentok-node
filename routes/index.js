80746db06776d50bcde47935ed9f0e1a900565b2
Lucas <31716292+yapyuyou@users.noreply.github.com>
2019-06-11 18:38:17 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -108,11 +108,8 @@ const VoiceResponse = require('twilio').twiml.VoiceResponse;
 router.get('/voice', function(req, res) {
   // Use the Twilio Node.js SDK to build an XML response
   const twiml = new VoiceResponse();
-  twiml.say({ voice: 'alice' }, 'hello world!');
-
-  // Render the response as XML in reply to the webhook request
-  response.type('text/xml');
-  response.send(twiml.toString());
+  twiml.say('Hello World');
+  
   console.log("yes");
   console.log(res.toString());
   //const connect = twiml.connect();
