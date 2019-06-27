820f7fa70b8bd0ef7d37a3dbced741332ebbf1a8
Lucas <31716292+yapyuyou@users.noreply.github.com>
2019-06-27 12:36:34 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -167,19 +167,13 @@ router.post('/voice', function (req, res) {
 });
 
 //Presence check for vaas demo
-router.get('twilio/participants', function (req, res) {
+router.get('/twilio/participants', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
-  res.send({
-        message: "VoicR-conference-calling",
-        token: "haha"
-      });
-//   res.send({
-//   list: Object.values(participants)
-//     });
+  res.send(Object.values(participants));
   });
 
 //Notify when leaving for vaas demo
-router.get('twilio/leave/:username', function (req, res) {
+router.get('/twilio/leave/:username', function (req, res) {
   var username = req.params.username;
   delete participants(username);
 });
